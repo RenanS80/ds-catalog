586a51c1edac0074d51108ab75428c0b46930719
Renan Soares <renansoaresffc@gmail.com>
2021-12-27 23:15:34 -0300

    ProductDetail responsive

--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -1,8 +1,36 @@
 import './styles.css';
 
-function ProductDetails(){
-    return(
-        <h1>Página product details</h1>
+import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
+import ProductPrice from 'components/ProductPrice';
+
+function ProductDetails() {
+    return (
+        <section className="product-details-container">
+            <div className="base-card product-details-card">
+                <div className="goback-container">
+                    <ArrowIcon />
+                    <h2>Voltar</h2>
+                </div>
+
+                <div className="row">
+                    <div className="col-xl-6">
+                        <div className="img-container">
+                            <img src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg" alt="Nome do produto" />
+                        </div>
+                        <div className="name-price-container">
+                            <h1>Nome do produto</h1>
+                            <ProductPrice price={2345.67} />
+                        </div>
+                    </div>
+                    <div className="col-xl-6">
+                        <div className="description-container">
+                            <h2>Descrição do produto</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos, quisquam dignissimos? Ea accusamus expedita aperiam quibusdam adipisci eveniet. Placeat, quas.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 }
 
